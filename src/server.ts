024cf55f03dacaf6d5df3f8e0c91a82a8066c6f6
itsmiraz <mirajulislamofficial044@gmail.com>
2023-11-23 09:48:15 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import config from './app/config';
+import app from './app';
+
+import mongoose from 'mongoose';
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  try {
+    await mongoose.connect(config.databaseUrl as string);
+
+    app.listen(config.port, () => {
+      console.log(`app is runngin on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
