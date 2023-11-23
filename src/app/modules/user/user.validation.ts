42de4dbda2ae84d0011bab42f3bd8e93f5c53a2c
itsmiraz <mirajulislamofficial044@gmail.com>
2023-11-23 15:01:05 +0600

    user zod validation added

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+
+const userNameValidationSchema = z.object({
+  firstName: z.string().min(1).max(255),
+  lastName: z.string().min(1).max(255),
+});
+
+const addressValidationSchama = z.object({
+  street: z.string().min(1).max(255),
+  city: z.string().min(1).max(255),
+  country: z.string().min(1).max(255),
+});
+
+const orderSchema = z.object({
+  productName: z.string().min(1).max(255),
+  price: z.number(),
+  quantity: z.number(),
+});
+
+const userValidationSchema = z.object({
+  userId: z.number().min(4),
+  username: z.string().min(1).max(255),
+  password: z.string().min(6).max(20),
+  fullName: userNameValidationSchema,
+  age: z.number(),
+  hobbies: z.array(z.string()),
+  address: addressValidationSchama,
+  orders: z.array(orderSchema),
+});
+export default userValidationSchema;
